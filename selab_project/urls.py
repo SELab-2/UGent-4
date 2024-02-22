f9c5099aaa11713b7b135de5fb96f3b99471e2b4
mathis2003 <mathisdw@icloud.com>
2024-02-22 16:21:36 +0000

    api routing

--- selab_project/urls.py
+++ selab_project/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from .views import main
+
 urlpatterns = [
+    path('api/', main),
     path('admin/', admin.site.urls),
 ]
