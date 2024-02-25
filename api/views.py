436f38bf6df5a723b86b8292a683ce2d59cb97f6
mathis2003 <mathisdw@icloud.com>
2024-02-25 12:18:53 +0000

    microsoft-identity-association

--- api/views.py
+++ api/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.http import JsonResponse
+
+def microsoft_association(request):
+    #return HttpResponse("yolo")
+    return JsonResponse({"associatedApplications": [{ "applicationId": "239ce609-e362-4cf6-919f-97e6935ef5f5" }]})
+
 def main(request):
     return HttpResponse("my http resonse")
