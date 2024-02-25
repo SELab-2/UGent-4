aa33a7acbfd421fabd11325c63b45b725002cc9b
Mathis De Witte <49711425+mathis2003@users.noreply.github.com>
2024-02-25 13:20:38 +0100

    delete comment

--- api/views.py
+++ api/views.py
@@ -4,7 +4,6 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 
 def microsoft_association(request):
-    #return HttpResponse("yolo")
     return JsonResponse({"associatedApplications": [{ "applicationId": "239ce609-e362-4cf6-919f-97e6935ef5f5" }]})
 
 def main(request):
