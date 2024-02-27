2a94a5d9872b2ca7f88cc92eb94f2de2d7a966a0
timon coucke <coucketimon@gmail.com>
2024-02-27 17:23:03 +0100

    Finished basic login system

--- api/views.py
+++ api/views.py
@@ -2,34 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from django.http import JsonResponse
-from django.conf import settings
-
-import requests
-
-def get_graph_token():
-    """
-        Get graph token from AD url.
-    """
-    try:
-        url = settings.AD_URL
-
-        headers = {'Content-Type': 'application/x-www-form-urlencoded', 'Accept': 'application/json'}
-
-        data = {
-            'grant_type': 'client_credentials',
-            'client_id': settings.CLIENT_ID,
-            'client_secret': settings.CLIENT_SECRET,
-            'scope': 'https://graph.microsoft.com/.default',
-        }
-
-        print('hallo')
-        response = requests.post(url=url, headers=headers, data=data)
-        print(response)
-        return response.json()
-    except:
-        return None
-
-
+from utils import get_graph_token
 
 
 def succesfully_logged_in(request):
@@ -37,14 +10,8 @@ def succesfully_logged_in(request):
         Get user details from microsoft graph apis.
     """
     graph_token = get_graph_token()
-    print(graph_token)
-    """if graph_token:
-        url = 'https://graph.microsoft.com/v1.0/users/' + request.user.username
 
-        headers = {
-            'Authorization': 'Bearer ' + graph_token
-        }"""
-    return HttpResponse("Logged in!")
+    return HttpResponse(f"Logged in as {request.user.first_name} {request.user.last_name}, with email: {request.user.username} \nWith token: {graph_token['access_token']}")
 
 def microsoft_association(request):
     return JsonResponse({"associatedApplications": [{ "applicationId": "239ce609-e362-4cf6-919f-97e6935ef5f5" }]})
