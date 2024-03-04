5650483958f3a3f6527efc4803bf92b3c261b3ad
ticoucke <timon.coucke@ugent.be>
2024-03-04 11:37:32 +0100

    Fixed small bug

--- api/views/views.py
+++ api/views/views.py
@@ -1,4 +1,4 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from api.utils import get_graph_token
 from django.shortcuts import redirect
 
@@ -9,9 +9,9 @@ def login_redirect(request):
     """
     graph_token = get_graph_token()
 
-    #HttpResponse(f"Logged in as {request.user.first_name} {request.user.last_name}, with email: {request.user.username} \nWith token: {graph_token['access_token']}")
+    return HttpResponse(f"Logged in as {request.user.first_name} {request.user.last_name}, with email: {request.user.username} \nWith token: {graph_token['access_token']}")
 
-    return redirect("https://sel2-4.be")
+    return redirect("https://sel2-4.ugent.be")
 
 def microsoft_association(request):
     return JsonResponse({"associatedApplications": [{ "applicationId": "239ce609-e362-4cf6-919f-97e6935ef5f5" }]})
