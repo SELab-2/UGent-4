a8697b5fe385caee16da545ee9109d41e1545176
Mathis De Witte <mathisdw@icloud.com>
2024-03-04 12:17:12 +0100

    dummy test

--- api/tests.py
+++ api/tests.py
@@ -4,13 +4,12 @@ from .models import Dummy
 class TestModels(TestCase):
 
     def setUp(self):
-        pass
-        #self.dummy1 = Dummy.objects.create(
-        #    name='Dummy 1',
-        #    budget=10000
-        #)
+        #pass
+        self.dummy1 = Dummy.objects.create(
+            name='Dummy 1',
+            budget=10000
+        )
     
     def test_dummy_model(self):
-        #self.assertEquals(self.dummy1.slug, 'dummy-1')
-        #self.assertTrue(isinstance(d, Score))
-        self.assertTrue(True)
+        self.assertEquals(self.dummy1.slug, 'dummy-1')
+        #self.assertTrue(True)
