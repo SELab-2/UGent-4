ed1fb3c755ff51c28b4ec7394a447c8264e3da32
Alexandre Paice <alexandre.paice@ugent.be>
2024-03-06 15:30:38 +0100

    views (get, post, put en delete)

--- api/views/indiening.py
+++ api/views/indiening.py
@@ -2,40 +2,42 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-
-
-from api.models import Student
-from api.serializers import StudentSerializer
-#from ..utils import json_error
+from ..models import Indiening
+from ..serializers import IndieningSerializer
 
 
 @api_view(['GET', 'POST'])
-def student_list(request):
+def indiening_list(request, format=None):
+
     if request.method == 'GET':
-        students = Student.objects.all()
-        serializer = StudentSerializer(students, many=True)
+        lesgevers = Indiening.objects.all()
+        serializer = IndieningSerializer(lesgevers, many=True)
         return Response(serializer.data)
+    
     elif request.method == 'POST':
-        serializer = StudentSerializer(data=request.data)
+        serializer = IndieningSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
-    
-@api_view(['GET', 'PUT'])
-def student_detail(request, id):
+@api_view(['GET', 'PUT', 'DELETE'])
+def indiening_detail(request, id, format=None): 
     try:
-        student = Student.objects.get(pk=id)
-    except Student.DoesNotExist:
+        indiening = Indiening.objects.get(pk=id)
+    except Indiening.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
-        serializer = StudentSerializer(student)
+        serializer = IndieningSerializer(indiening)
         return Response(serializer.data)
+    
     elif request.method == 'PUT':
-        serializer = StudentSerializer(student, data=request.data)
+        serializer = IndieningSerializer(indiening, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        indiening.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- api/views/vak.py
+++ api/views/vak.py
@@ -2,18 +2,42 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from api.models.vak import Vak
-from api.serializers.vak import VakSerializer
+from ..models import Vak
+from ..serializers import VakSerializer
 
 
 @api_view(['GET', 'POST'])
-def lesgever_list(request):
+def vak_list(request, format=None):
+
     if request.method == 'GET':
         lesgevers = Vak.objects.all()
         serializer = VakSerializer(lesgevers, many=True)
-        return JsonResponse({'lesgevers': serializer.data})
+        return Response(serializer.data)
+    
     elif request.method == 'POST':
         serializer = VakSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+@api_view(['GET', 'PUT', 'DELETE'])
+def vak_detail(request, id, format=None): 
+    try:
+        vak = Vak.objects.get(pk=id)
+    except Vak.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = VakSerializer(vak)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = VakSerializer(vak, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        vak.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
