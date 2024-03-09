88fc6225fe755194efb21bdd0949d591df7143a5
arallaer <arnout.allaert@ugent.be>
2024-03-09 18:02:57 +0100

    gebruiker serializer tests

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -3,7 +3,6 @@ from django.urls import reverse
 
 class TestViews(TestCase):
     def test_should_show_register_page(self):
-        print('nigger')
         #self.client.get(reverse('register'))
         #self.assertEqual(response.status_code, 200)
         #self.assertTemplateUsed(response, "authentication/register")
