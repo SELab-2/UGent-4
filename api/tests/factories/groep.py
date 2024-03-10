0169a8273ffb05f7b47cce68cb044331341c50a0
arallaer <arnout.allaert@ugent.be>
2024-03-10 19:45:48 +0100

    groep tests + factories

---
+++ api/tests/factories/groep.py
@@ -0,0 +1,23 @@
+import factory
+from api.models.groep import Groep
+from api.models.project import Project
+from api.tests.factories.gebruiker import GebruikerFactory
+from api.tests.factories.project import ProjectFactory
+
+
+class GroepFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Groep
+
+    project = factory.SubFactory(ProjectFactory)
+
+    @factory.post_generation
+    def students(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            for student in extracted:
+                self.students.add(student)
+        else:
+            self.students.add(GebruikerFactory())
