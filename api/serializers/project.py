25169913cde1f5645e3eac267c576bf8cdeebbf3
Alexandre Paice <alexandre.paice@ugent.be>
2024-03-10 13:49:25 +0100

    serializers afgewerkt

--- api/serializers/project.py
+++ api/serializers/project.py
@@ -1,8 +1,34 @@
 from rest_framework import serializers
 from api.models.project import Project
 
+from datetime import datetime
+
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = '__all__'
+
+    def create(self, validated_data):
+        deadline = validated_data.pop('deadline')
+
+        validate_deadline(deadline)
+
+        project = Project.objects.create(**validated_data)
+        project.deadline.set(deadline)
+        return project
+    
+    def update(self, instance, validated_data):
+        deadline = validated_data.pop('deadline')
+
+        validate_deadline(deadline)
+
+        instance = Project.objects.create(**validated_data)
+        instance.deadline.set(deadline)
+        instance.save()
+        return instance
+    
+
+def validate_deadline(deadline):
+    if deadline <= datetime.now():
+        raise serializers.ValidationError("Deadline moet in de toekomst liggen")
