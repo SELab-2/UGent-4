9e8174d13f97a1e79a2ce9894187c15ad3227b40
Gus Vanpoucke <gus.vanpoucke@ugent.be>
2024-03-19 14:45:08 +0100

    internationalisation scores page

--- frontend/frontend/src/pages/scoresPage/ProjectScoresPage.tsx
+++ frontend/frontend/src/pages/scoresPage/ProjectScoresPage.tsx
@@ -2,6 +2,7 @@ import { Header } from "../../components/Header";
 import { Box, Button, Stack } from "@mui/material";
 import { useParams } from "react-router-dom";
 import { StudentsView } from "./StudentsView.tsx";
+import {t} from "i18next";
 import SaveIcon from '@mui/icons-material/Save';
 import CloseIcon from '@mui/icons-material/Close';
 
@@ -17,7 +18,7 @@ export function ProjectScoresPage() {
                 </Box>
                 <Box display="flex" flexDirection="row" sx={{ width: '100%', height:"auto", marginTop:10 }}>
                     <Box display="flex" flexDirection="row" sx={{ width: '50%', height:"auto" }}>
-                        <Button variant="contained" color="secondary">Exporteer Indieningen</Button>
+                        <Button variant="contained" color="secondary">{t("export_submissions")}</Button>
                     </Box>
                     <Box display="flex" flexDirection="row-reverse" sx={{ width: '50%', height:"auto" }}>
                         <Button variant="contained" startIcon={<SaveIcon />}/>
