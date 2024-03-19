ec13f1060109b48f88ac98ee5224660842e4cfb4
Gus Vanpoucke <gus.vanpoucke@ugent.be>
2024-03-19 17:08:55 +0100

    simple routing using buttons

--- frontend/frontend/src/pages/SubjectsPage/SubjectsTeacherPage.tsx
+++ frontend/frontend/src/pages/SubjectsPage/SubjectsTeacherPage.tsx
@@ -2,23 +2,31 @@ import { Header } from "../../components/Header";
 import { Box, IconButton, Stack } from "@mui/material";
 import TabSwitcher from "../../components/TabSwitcher.tsx";
 import {ProjectsView} from "./ProjectsView.tsx";
-import { useParams } from "react-router-dom";
+import { redirect, useNavigate, useParams } from "react-router-dom";
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 
 export function SubjectsTeacherPage() {
     let { courseId } = useParams();
     courseId = String(courseId);
+
+    const navigate = useNavigate();
+
+    const addProject = () => {
+        console.log("add project");
+        navigate("/add_change_assignment");
+    }
+
     return (
         <>
             <Stack direction={"column"} spacing={10} sx={{width:"100%" ,height:"100%", backgroundColor:"background.default"}}>
-                <Header variant={"default"} title={"Naam Vak"} />
+                <Header variant={"editable"} title={"Naam Vak"} />
                 <Box sx={{ width: '100%', height:"70%", marginTop:10 }}>
                     <TabSwitcher titles={["current_projects","archived"]}
                                  nodes={[<ProjectsView courseId={courseId} isStudent={false} archived={false} />,
                                  <ProjectsView courseId={courseId} isStudent={false} archived={true} />]}/>
                 </Box>
                 <Box display="flex" flexDirection="row-reverse" sx={{ width: '100%', height:"30%" }}>
-                    <IconButton color="primary" edge="end" aria-label="add-project" >
+                    <IconButton onClick={addProject} color="primary" edge="end" aria-label="add-project" >
                         <AddCircleIcon sx={{fontSize: 60, height: "100%"}} />
                     </IconButton>
                 </Box>
--- frontend/frontend/src/pages/scoresPage/ProjectScoresPage.tsx
+++ frontend/frontend/src/pages/scoresPage/ProjectScoresPage.tsx
@@ -1,6 +1,6 @@
 import { Header } from "../../components/Header";
 import { Box, Button, Stack } from "@mui/material";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { StudentsView } from "./StudentsView.tsx";
 import {t} from "i18next";
 import SaveIcon from '@mui/icons-material/Save';
@@ -9,6 +9,23 @@ import CloseIcon from '@mui/icons-material/Close';
 export function ProjectScoresPage() {
     let { projectId } = useParams();
     projectId = String(projectId);
+
+    const navigate = useNavigate();
+
+    const exportSubmissions = () => {
+        console.log("export submissions");
+    }
+
+    const saveScores = () => {
+        console.log("save scores");
+        navigate("/assignment_teacher");
+    }
+
+    const deleteScores = () => {
+        console.log("delete scores");
+        navigate("/assignment_teacher");
+    }
+    
     return (
         <>
             <Stack direction={"column"} spacing={10} sx={{width:"100%" ,height:"100%", backgroundColor:"background.default"}}>
@@ -18,11 +35,11 @@ export function ProjectScoresPage() {
                 </Box>
                 <Box display="flex" flexDirection="row" sx={{ width: '100%', height:"auto", marginTop:10 }}>
                     <Box display="flex" flexDirection="row" sx={{ width: '50%', height:"auto" }}>
-                        <Button variant="contained" color="secondary">{t("export_submissions")}</Button>
+                        <Button onClick={exportSubmissions} variant="contained" color="secondary">{t("export_submissions")}</Button>
                     </Box>
                     <Box display="flex" flexDirection="row-reverse" sx={{ width: '50%', height:"auto" }}>
-                        <Button variant="contained" startIcon={<SaveIcon />}/>
-                        <Button variant="contained" color="secondary" startIcon={<CloseIcon />}/>
+                        <Button onClick={saveScores} variant="contained" startIcon={<SaveIcon />}/>
+                        <Button onClick={deleteScores} variant="contained" color="secondary" startIcon={<CloseIcon />}/>
                     </Box>
                 </Box>
             </Stack>
