0a686feecf4874e67597d9d499cb601795165b2a
Ben De Meurichy <ben.demeurichy@ugent.be>
2024-03-29 21:24:04 +0100

    beetje configuratie en integratie api requests toegevoegd

--- frontend/frontend/src/axiosConfig.ts
+++ frontend/frontend/src/axiosConfig.ts
@@ -22,8 +22,31 @@ export function getVakken() { // mogelijk/nodig?
     return instance.get('/vakken');
 }
 
+export function getVak(vakId: string) {
+    return instance.get('/vakken/' + vakId);
+}
+
+export function addVak(assignment: assignment) {
+    return instance.post('/vakken', assignment);
+}
+
+//mogelijk/nodig?
+export function changeVak(vakId: string, assignment: assignment) {
+    return instance.put('/vakken/' + vakId, assignment);
+}
+
+//mogelijk/nodig?
+export function deleteVak(vakId: string) {
+    return instance.delete('/vakken/' + vakId);
+}
+
 export function getProjecten() {
     return instance.get('/projecten');
+}
+
+//mogelijk/nodig?
+export function getProject(projectId: string) {
+    return instance.get('/projecten/' + projectId);
 }
 
 export default instance;
