60e2dfccefa0494e5f892394f62bc1b6b96d7d3f
BenDeMeurichy <48103261+Bendemeurichy@users.noreply.github.com>
2024-03-30 23:20:05 +0100

    oude authsecrets verwijderd

--- frontend/frontend/src/authConfig/authSecrets.ts
+++
@@ -1,7 +0,0 @@
-const AuthSecrets = {
-    clientId: 'e9b4d537-624d-4faf-8122-5416f8ce1469', // This is the ONLY mandatory field that you need to supply.
-    authority: 'https://login.microsoftonline.com/d7811cde-ecef-496c-8f91-a1786241b99c', // Replace the placeholder with your tenant subdomain
-
-};
-
-export default AuthSecrets;
