9064ad0bcfaa0487906766bc4e28d8442f8ebd52
Mathis De Witte <mathisdw@icloud.com>
2024-03-31 18:27:54 +0200

    setup restricties UI

---
+++ frontend/frontend/src/pages/assignmentPage/AddRestrictionButton.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog, { DialogProps } from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import AddIcon from '@mui/icons-material/Add';
+import RestrictionsDialog from './RestrictionsDialog';
+
+
+
+export default function AddRestrictionButton() {
+  const [open, setOpen] = React.useState(false);
+  const [scroll, setScroll] = React.useState<DialogProps['scroll']>('paper');
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const descriptionElementRef = React.useRef<HTMLElement>(null);
+  React.useEffect(() => {
+    if (open) {
+      const { current: descriptionElement } = descriptionElementRef;
+      if (descriptionElement !== null) {
+        descriptionElement.focus();
+      }
+    }
+  }, [open]);
+
+  return (
+    <React.Fragment>
+      <Button 
+        sx={{bgcolor: 'secondary.main'}} 
+        onClick={() => {
+            setOpen(true);
+            setScroll('paper');
+        }}>
+            <AddIcon sx={{color: "secondary.contrastText"}}></AddIcon>
+      </Button>
+    <Dialog
+        open={open}
+        onClose={handleClose}
+        scroll={scroll}
+        aria-labelledby="scroll-dialog-title"
+        aria-describedby="scroll-dialog-description"
+    >
+        <DialogTitle id="scroll-dialog-title">Add restriction</DialogTitle>
+        <DialogContent dividers={scroll === 'paper'}>
+                <RestrictionsDialog onClose={handleClose}></RestrictionsDialog>
+        </DialogContent>
+        <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+        </DialogActions>
+    </Dialog>
+    </React.Fragment>
+  );
+}
