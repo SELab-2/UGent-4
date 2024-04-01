78d7c77415de4f73af7dc0ce7adeca1c9b031b5a
lgdtimtou <coucketimon@gmail.com>
2024-04-01 14:31:38 +0200

    Kan nu checken als testen gefaald of geslaagd zijn in python entrypoint

--- api/docker/python_entrypoint.py
+++ api/docker/python_entrypoint.py
@@ -4,4 +4,4 @@ import os
 def run_tests_on(indiening_id, project_id):
     stream = os.popen(f"bash rundocker.sh {indiening_id} {project_id}")
     output = stream.read()
-    return (": FAIL" in output, output)
+    return ": FAIL" in output, output
