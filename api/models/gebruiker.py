99071829fdf53818a05bff2e2af11fbf31f38ae0
lgdtimtou <coucketimon@gmail.com>
2024-04-09 15:05:19 +0200

    gepinde_vakken toevoegen aan gebruiker model, met updates aan serializers voor validatie checks etc

--- api/models/gebruiker.py
+++ api/models/gebruiker.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from api.models.vak import Vak
 
 
 class Gebruiker(models.Model):
@@ -19,6 +20,7 @@ class Gebruiker(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     is_lesgever = models.BooleanField(default=False)
+    gepinde_vakken = models.ManyToManyField(Vak, related_name="gebruiker_gepinde_vakken", blank=True)
 
     def __str__(self):
         return self.user.first_name + " " + self.user.last_name
