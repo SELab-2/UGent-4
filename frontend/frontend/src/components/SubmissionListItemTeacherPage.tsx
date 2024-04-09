f2c47b69b1938c34773c005de3ee2535a36c2472
Alexandre Paice <alexandre.paice@ugent.be>
2024-04-09 18:55:37 +0200

    score en status werken correct

--- frontend/frontend/src/components/SubmissionListItemTeacherPage.tsx
+++ frontend/frontend/src/components/SubmissionListItemTeacherPage.tsx
@@ -1,6 +1,8 @@
 import {ListItem, ListItemText, ListItemIcon, ListItemButton} from "@mui/material";
 import {useNavigate} from "react-router-dom";
 import DownloadIcon from '@mui/icons-material/Download';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 import { useEffect, useState } from "react";
 import instance from "../axiosConfig";
 
@@ -35,8 +37,9 @@ export function SubmissionListItemTeacherPage({group_id}:SubmissionListItemTeach
                 const lastSubmission = submissionsResponse.data[submissionsResponse.data.length - 1];
                 setSubmitted(lastSubmission);
                 if (lastSubmission){
-                    const scoreResponse = await instance.get(`/scores/?indiening=${submitted.indiening_id}`);
-                    setScore(scoreResponse.data);
+                    const scoreResponse = await instance.get(`/scores/?indiening=${lastSubmission.indiening_id}`);
+                    setScore(scoreResponse.data[scoreResponse.data.length - 1]);
+                    console.log(score.score);
                 }
             } catch (error) {
                 console.error("Error fetching data:", error);
@@ -72,7 +75,13 @@ export function SubmissionListItemTeacherPage({group_id}:SubmissionListItemTeach
                         primary={group_id}
                     />
                     <ListItemText sx={{ maxWidth: 100 }} primary={submitted? new Date(submitted.tijdstip).toLocaleDateString() : "-"} />
-                    <ListItemText sx={{ maxWidth: 50 }} primary={score? Number(score.score) + "/20" : "-"} />
+                    <ListItemText sx={{ maxWidth: 50 }} primary={score? `${Number(score.score)}` + "/20" : "-"} />
+                    <ListItemIcon sx={{ minWidth: 35 }}>
+                        {submitted?.status ? (
+                        <HighlightOffIcon sx={{ color: "error.main" }} />) : 
+                        (submitted !== undefined && <CheckCircleOutlineIcon sx={{ color: "success.main" }} />
+                        )}
+                    </ListItemIcon>
                     <ListItemIcon sx={{ minWidth: 35 }}>
                         {submitted ? (
                             <DownloadIcon
