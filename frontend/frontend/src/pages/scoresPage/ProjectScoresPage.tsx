42e327f8ccc127745d2fd797317d1c2d1bae1c07
Ben De Meurichy <ben.demeurichy@ugent.be>
2024-04-10 21:14:33 +0200

    automatische fix eslint

--- frontend/frontend/src/pages/scoresPage/ProjectScoresPage.tsx
+++ frontend/frontend/src/pages/scoresPage/ProjectScoresPage.tsx
@@ -1,13 +1,13 @@
-import { Header } from "../../components/Header";
-import { Box, Button, Stack } from "@mui/material";
-import { useNavigate, useParams } from "react-router-dom";
-import { StudentsView } from "./StudentsView.tsx";
+import {Header} from "../../components/Header";
+import {Box, Button, Stack} from "@mui/material";
+import {useNavigate, useParams} from "react-router-dom";
+import {StudentsView} from "./StudentsView.tsx";
 import {t} from "i18next";
 import SaveIcon from '@mui/icons-material/Save';
 import CloseIcon from '@mui/icons-material/Close';
 
 export function ProjectScoresPage() {
-    let { assigmentId } = useParams();
+    const {assigmentId} = useParams();
     const projectId = Number(assigmentId);
 
     const navigate = useNavigate();
@@ -29,22 +29,25 @@ export function ProjectScoresPage() {
         console.log("delete scores");
         navigate("/assignment_teacher");
     }
-    
+
     return (
         <>
-            <Stack direction={"column"} spacing={0} sx={{width:"100%" ,height:"100%", backgroundColor:"background.default"}}>
-                <Header variant={"default"} title={"Project 1: Scores"} />
-                <Box sx={{ width: '100%', height:"70%", marginTop:10, boxShadow: 3, borderRadius: 3 }}>
-                    <StudentsView projectId={projectId} />
+            <Stack direction={"column"} spacing={0}
+                   sx={{width: "100%", height: "100%", backgroundColor: "background.default"}}>
+                <Header variant={"default"} title={"Project 1: Scores"}/>
+                <Box sx={{width: '100%', height: "70%", marginTop: 10, boxShadow: 3, borderRadius: 3}}>
+                    <StudentsView projectId={projectId}/>
                 </Box>
-                <Box display="flex" flexDirection="row" sx={{ width: '100%', height:"30%", marginTop:5 }}>
-                    <Box display="flex" flexDirection="row" sx={{ width: '50%', height:"auto" }}>
-                        <Button onClick={exportSubmissions} variant="contained" color="secondary">{t("export_submissions")}</Button>
-                        <Button onClick={uploadScores} variant="contained" color="secondary">{t("export_submissions")}</Button>
+                <Box display="flex" flexDirection="row" sx={{width: '100%', height: "30%", marginTop: 5}}>
+                    <Box display="flex" flexDirection="row" sx={{width: '50%', height: "auto"}}>
+                        <Button onClick={exportSubmissions} variant="contained"
+                                color="secondary">{t("export_submissions")}</Button>
+                        <Button onClick={uploadScores} variant="contained"
+                                color="secondary">{t("export_submissions")}</Button>
                     </Box>
-                    <Box display="flex" flexDirection="row-reverse" sx={{ width: '50%', height:"auto" }}>
-                        <Button onClick={saveScores} variant="contained" startIcon={<SaveIcon />}/>
-                        <Button onClick={deleteScores} variant="contained" color="secondary" startIcon={<CloseIcon />}/>
+                    <Box display="flex" flexDirection="row-reverse" sx={{width: '50%', height: "auto"}}>
+                        <Button onClick={saveScores} variant="contained" startIcon={<SaveIcon/>}/>
+                        <Button onClick={deleteScores} variant="contained" color="secondary" startIcon={<CloseIcon/>}/>
                     </Box>
                 </Box>
             </Stack>
