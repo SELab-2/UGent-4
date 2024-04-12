1d3ab22c00ee12e093cdf64dbfcf09eb871078c3
Robbe <robbe.schildermans@ugent.be>
2024-04-12 13:08:47 +0200

    vakken editten werkt

--- frontend/frontend/src/i18n/en.ts
+++ frontend/frontend/src/i18n/en.ts
@@ -73,6 +73,7 @@ const english = {
     archive_project_warning: "Archive Project?",
     cant_be_undone: "This cannot be undone.",
     archive: "Archive",
+    save: "Save",
 };
 
 export default english;
