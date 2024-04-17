709ef4fc87df119586827618391dbfd00bd9993a
elias <Elias.Meynckens@UGent.be>
2024-04-17 21:29:56 +0200

    applied prettier to all files

--- frontend/frontend/src/axiosConfig.ts
+++ frontend/frontend/src/axiosConfig.ts
@@ -1,23 +1,20 @@
-import axios from 'axios';
-
+import axios from 'axios'
 
 const instance = axios.create({
     baseURL: 'https://sel2-4.ugent.be/api/',
     //baseURL: 'http://localhost:8000/api/',
-    headers: {'Content-Type': 'application/json'}
-});
+    headers: { 'Content-Type': 'application/json' },
+})
 
 //TODO constant logging better only for development
 instance.interceptors.request.use((request) => {
-    console.log(request);
-    return request;
-});
+    console.log(request)
+    return request
+})
 
 instance.interceptors.response.use((response) => {
-    console.log(response);
+    console.log(response)
     return response
-});
+})
 
-
-
-export default instance;
+export default instance
--- frontend/frontend/src/components/ItemList.tsx
+++ frontend/frontend/src/components/ItemList.tsx
@@ -1,40 +1,64 @@
-import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from '@mui/material';
-import theme from '../Theme';
-import {FC} from "react";
+import {
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+} from '@mui/material'
+import theme from '../Theme'
+import { FC } from 'react'
 
 interface Item {
-    opdracht: string;
-    deadline: string;
-    status: string;
-    score: string;
+    opdracht: string
+    deadline: string
+    status: string
+    score: string
 }
 
 interface ItemListProps {
-    itemList: Item[];
+    itemList: Item[]
 }
 
-export const ItemList: FC<ItemListProps> = ({itemList}) => {
+export const ItemList: FC<ItemListProps> = ({ itemList }) => {
     return (
         <TableContainer component={Paper} variant="outlined">
             <Table>
                 <TableHead>
                     <TableRow>
-                        <TableCell style={{
-                            backgroundColor: theme.palette.secondary.main,
-                            color: theme.palette.secondary.contrastText
-                        }}>Opdracht</TableCell>
-                        <TableCell style={{
-                            backgroundColor: theme.palette.secondary.main,
-                            color: theme.palette.secondary.contrastText
-                        }}>Deadline</TableCell>
-                        <TableCell style={{
-                            backgroundColor: theme.palette.secondary.main,
-                            color: theme.palette.secondary.contrastText
-                        }}>Status</TableCell>
-                        <TableCell style={{
-                            backgroundColor: theme.palette.secondary.main,
-                            color: theme.palette.secondary.contrastText
-                        }}>Score</TableCell>
+                        <TableCell
+                            style={{
+                                backgroundColor: theme.palette.secondary.main,
+                                color: theme.palette.secondary.contrastText,
+                            }}
+                        >
+                            Opdracht
+                        </TableCell>
+                        <TableCell
+                            style={{
+                                backgroundColor: theme.palette.secondary.main,
+                                color: theme.palette.secondary.contrastText,
+                            }}
+                        >
+                            Deadline
+                        </TableCell>
+                        <TableCell
+                            style={{
+                                backgroundColor: theme.palette.secondary.main,
+                                color: theme.palette.secondary.contrastText,
+                            }}
+                        >
+                            Status
+                        </TableCell>
+                        <TableCell
+                            style={{
+                                backgroundColor: theme.palette.secondary.main,
+                                color: theme.palette.secondary.contrastText,
+                            }}
+                        >
+                            Score
+                        </TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
@@ -49,5 +73,5 @@ export const ItemList: FC<ItemListProps> = ({itemList}) => {
                 </TableBody>
             </Table>
         </TableContainer>
-    );
-};
+    )
+}
