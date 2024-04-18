074c2a182312ae6c04a41765d321fd2e7613b91f
Ben De Meurichy <ben.demeurichy@ugent.be>
2024-04-18 18:00:09 +0200

    restrictioncard

--- frontend/frontend/src/components/RestrictionCard.tsx
+++ frontend/frontend/src/components/RestrictionCard.tsx
@@ -1,29 +1,38 @@
-import {restriction} from "../pages/addChangeAssignmentPage/AddChangeAssignmentPage.tsx";
-import {Box, IconButton, Typography} from "@mui/material";
-import CloseIcon from "@mui/icons-material/Close";
-
+import { restriction } from '../pages/addChangeAssignmentPage/AddChangeAssignmentPage.tsx'
+import { Box, IconButton, Typography } from '@mui/material'
+import CloseIcon from '@mui/icons-material/Close'
 
 interface RestrictionCardProps {
-    restriction: restriction;
-    restrictions: restriction[];
-    setRestrictions: (restriction: restriction[]) => void;
+    restriction: restriction
+    restrictions: restriction[]
+    setRestrictions: (restriction: restriction[]) => void
 }
 
-export function RestrictionCard({restriction, restrictions, setRestrictions}: RestrictionCardProps) {
+export function RestrictionCard({
+    restriction,
+    restrictions,
+    setRestrictions,
+}: RestrictionCardProps) {
     //handle the removal of the restriction from the list
     const handleRemove = () => {
-        setRestrictions(restrictions.filter((r) => r !== restriction));
+        setRestrictions(restrictions.filter((r) => r !== restriction))
     }
-
 
     return (
         <>
-            <Box display={'flex'} flexDirection={'row'} width={'100%'} justifyContent={'space-between'}>
-                <Typography variant={'body2'}>{restriction.script.replace(/^.*[\\/]/, '')}</Typography>
+            <Box
+                display={'flex'}
+                flexDirection={'row'}
+                width={'100%'}
+                justifyContent={'space-between'}
+            >
+                <Typography variant={'body2'}>
+                    {restriction.script.replace(/^.*[\\/]/, '')}
+                </Typography>
                 <IconButton onClick={handleRemove}>
-                    <CloseIcon/>
+                    <CloseIcon />
                 </IconButton>
             </Box>
         </>
-    );
+    )
 }
