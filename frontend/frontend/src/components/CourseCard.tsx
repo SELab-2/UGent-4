be98de4379f32ef25cf89e495c2a1fe6aa1674a1
Gus Vanpoucke <159711661+gusvanpoucke@users.noreply.github.com>
2024-04-27 13:57:28 +0200

    leerkrachten kunnen nu wel onzichtbare projecten zien op mainpage

--- frontend/frontend/src/components/CourseCard.tsx
+++ frontend/frontend/src/components/CourseCard.tsx
@@ -312,10 +312,6 @@ export function CourseCard({ courseId, archived, isStudent }: CourseCardProps) {
                                             >
                                                 <List disablePadding={true}>
                                                     {assignments
-                                                        .filter(
-                                                            (assignment) =>
-                                                                assignment.zichtbaar
-                                                        )
                                                         .map((assignment) => (
                                                             <AssignmentListItem
                                                                 key={
