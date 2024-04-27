cc86c10c486f82ac988c886f949d11901d7891e9
Mathis De Witte <mathisdw@icloud.com>
2024-04-27 22:32:14 +0200

    documentatie AddChangeSubjectPage

---
+++ frontend/frontend/cypress/page/subjectsPage/SubjectsPage.cy.tsx
@@ -0,0 +1,15 @@
+import { SubjectsPage } from '../../../src/pages/subjectsPage/SubjectsPage';
+import { BrowserRouter } from 'react-router-dom';
+
+
+describe('SubjectsPage', () => {
+    it('renders the SubjectsPage', () => {
+        // Mount the SubjectsPage component within a BrowserRouter
+        cy.mount(`<BrowserRouter><SubjectsPage /></BrowserRouter>`);
+        
+        // You may want to add assertions here to verify the rendered output
+        // For example, check if certain elements exist in the rendered output
+        // cy.get('button').should('exist');
+        // cy.get('ul').should('exist');
+    });
+});
