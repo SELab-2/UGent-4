b0166cd59163be29c126056a786af7fe7c35481a
Mathis De Witte <mathisdw@icloud.com>
2024-05-05 17:34:10 +0200

    documentatie templates UI

--- frontend/frontend/src/pages/addChangeAssignmentPage/RestrictionTemplateUI.tsx
+++ frontend/frontend/src/pages/addChangeAssignmentPage/RestrictionTemplateUI.tsx
@@ -18,11 +18,17 @@ export default function RestrictionTemplateUI({restrictionCode} : {restrictionCo
 
     const params = parseParams(restrictionCode);
 
+    // TODO: als er meerdere parameters van het type array zijn, 
+    // dan worden die allemaal samengezet, wat natuurlijk niet de bedoeling is.
+    // Waarschijnlijk zullen de values pas uit de parameter gehaald moeten worden in de code.
+    // Het probleem wordt dan wel weer hoe de onderste functies aangepast moeten worden.
+    // Want elke array moet een eigen state hebben, en die moet dan ook weer aangepast worden.
     const initialArrayValues = params.filter(param => param.type === 'array').map(param => {
         return param.value;
     });
     const [arrayValues, setArrayValues] = React.useState([...initialArrayValues.map(value => ({ value }))]);
 
+    // All functions beneath are for handling the array type of parameters.
     const handleArrayChange = (index: number, event) => {
         const newArrayValues = [...arrayValues];
         newArrayValues[index].value = event.target.value;
@@ -44,12 +50,13 @@ export default function RestrictionTemplateUI({restrictionCode} : {restrictionCo
             {params.map((param, index) => (
                 <div key={index} style={{ marginBottom: '20px' }}>
                     {param.type === 'number' && (
+                        // In this case we have a number
+                        // It needs the appropriate input field
                         <>
                             <Typography variant={'body2'}>
                                 {param.description}
                             </Typography>
                             <TextField
-                                //label={param.description}
                                 type='number'
                                 value={param.value}
                                 fullWidth
@@ -58,6 +65,8 @@ export default function RestrictionTemplateUI({restrictionCode} : {restrictionCo
                         
                     )}
                     {param.type === 'string' && (
+                        // In this case we have a string
+                        // It needs the appropriate input field
                         <>
                             <Typography variant={'body2'}>
                                 {param.description}
@@ -72,6 +81,8 @@ export default function RestrictionTemplateUI({restrictionCode} : {restrictionCo
                         
                     )}
                     {param.type === 'boolean' && (
+                        // In this case we have a boolean
+                        // It needs the appropriate input field
                         <>
                             <Typography variant={'body2'}>
                                 {param.description}
@@ -86,6 +97,9 @@ export default function RestrictionTemplateUI({restrictionCode} : {restrictionCo
                         
                     )}
                     {param.type === 'array' && (
+                        // In this case we have a list
+                        // It needs to have a listing of the present elements,
+                        // as well as a button to add entries to the list.
                         <div>
                             <Typography variant={'body2'}>
                                 {param.description}
@@ -116,21 +130,22 @@ export default function RestrictionTemplateUI({restrictionCode} : {restrictionCo
     );
 }
 
+// Helper function for parsing a value in either python or bash code.
 function parseValue(value: string): string | number | boolean | any[] {
     if (value.startsWith('"') && value.endsWith('"')) {
         return value.slice(1, -1);
-    } else if (value === 'True' || value === 'true') {
+    } else if (value === 'True' || value === 'true') { // capital T for python or lowercase t for bash
         return true;
-    } else if (value === 'False' || value === 'false') {
+    } else if (value === 'False' || value === 'false') { // capital F for python or lowercase f for bash
         return false;
     } else if (!isNaN(parseInt(value))) {
         return parseInt(value);
-    } else if (value.startsWith('[') && value.endsWith(']')) {
+    } else if (value.startsWith('[') && value.endsWith(']')) { // square brackets for python lists
         // Python-style list
-        return value.slice(1, -1).split(',').map(item => parseValue(item.trim()));
-    } else if (value.startsWith('(') && value.endsWith(')')) {
+        return value.slice(1, -1).split(',').map(item => parseValue(item.trim())); // in python the values are separated by commas
+    } else if (value.startsWith('(') && value.endsWith(')')) { // round brackets for bash lists
         // Bash-style list
-        return value.slice(1, -1).split(' ').map(item => parseValue(item.trim()));
+        return value.slice(1, -1).split(' ').map(item => parseValue(item.trim())); // in bash the values are separated by whitespace
     } else {
         return value;
     }
@@ -145,6 +160,7 @@ function parseParams(code: string) {
     for (let i = 0; i < lines.length; i++) {
         const line = lines[i].trim();
 
+        // Look for the #@param tag
         if (line.startsWith('#@param')) {
             description = lines[i + 1].trim().replace(/^# ?/, '');
             const variableLine = lines[i + 2].trim();
@@ -155,9 +171,6 @@ function parseParams(code: string) {
             params.push({type: Array.isArray(parsedValue) ? 'array' : typeof(parsedValue), description, variable, value: parsedValue});
         }
     }
-
-    console.log("params length:");
-    console.log(params.length);
 
     return params;
 }
