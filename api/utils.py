4eab0391f76c9156464296254667a64b0ff2bb1e
lgdtimtou <coucketimon@gmail.com>
2024-05-15 09:39:02 +0200

    Bevestigingsmail afgewerkt

--- api/utils.py
+++ api/utils.py
@@ -116,8 +116,6 @@ def get_gebruiker(user):
 
 
 def send_indiening_confirmation_mail(indiening):
-
-    receiver = "test-7lnkcgnm7@srv1.mail-tester.com"
     project = indiening.groep.project
 
     project_url = f"https://sel2-4.ugent.be/course/{project.vak.vak_id}/assignment/{project.project_id}"
@@ -125,23 +123,33 @@ def send_indiening_confirmation_mail(indiening):
     
     for student in indiening.groep.studenten.all():
         subject = "Indieningsontvangst"
-        body = f"""
+
+        email = MIMEMultipart("alternative")
+        email["Subject"] = subject
+        email["From"] = mail_username
+        email["To"] = student.user.email
+
+        plain_text = f"""
+        Beste {student.user.first_name} {student.user.last_name},
+        
+        Dit is een bevestiging dat uw indiening voor het project {project.titel} is ontvangen.
+        De indiening {indiening_status[indiening.status]}.
+        """
+
+        html_text = f"""
         <html>
         <body>
         <p>Beste {student.user.first_name} {student.user.last_name}</p>
-        <p>Dit is een bevestiging dat uw indiening voor <a href="{project_url}">{project.titel}</a> is ontvangen.</p>
+        <p>Dit is een bevestiging dat uw indiening voor het project <a href="{project_url}">{project.titel}</a> is ontvangen.</p>
         <p>De <a href="{indiening_url}">indiening</a> {indiening_status[indiening.status]}</p>
         </body>
         </html>
         """
 
+        email.attach(MIMEText(plain_text, "plain"))
+        email.attach(MIMEText(html_text, "html"))
 
-        email =  MIMEText(body, "html")
-        email["Subject"] = subject
-        email["From"] = mail_username
-        email["To"] = receiver#student.user.email
-
-        context = ssl.create_default_context()    
+        context = ssl.create_default_context()
         with smtplib.SMTP_SSL(smtp_server_address, smtp_port, context=context) as smtp_server:
             smtp_server.login(mail_username, mail_app_password)
-            smtp_server.sendmail(mail_username, receiver, email.as_string())
+            smtp_server.sendmail(mail_username, student.user.email, email.as_string())
