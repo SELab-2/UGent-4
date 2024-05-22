7f538f56dbfde63592c7a07651a2f7a3f0f330ab
arallaer <arnout.allaert@ugent.be>
2024-05-22 23:08:57 +0200

    update testen

--- api/tests/views/test_gebruiker.py
+++ api/tests/views/test_gebruiker.py
@@ -2,6 +2,9 @@ from rest_framework.test import APIClient, APITestCase
 from api.tests.factories.gebruiker import GebruikerFactory
 from django.urls import reverse
 from rest_framework import status
+from api.tests.factories.vak import VakFactory
+from api.models.gebruiker import Gebruiker
+from django.db import transaction
 
 
 class GebruikerListViewTest(APITestCase):
@@ -16,7 +19,7 @@ class GebruikerListViewTest(APITestCase):
         self.client.force_login(self.student.user)
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
+        self.assertEqual(len(response.data), Gebruiker.objects.count())
 
     def test_gebruiker_list_get_lesgevers(self):
         self.client.force_login(self.teacher.user)
@@ -50,6 +53,9 @@ class GebruikerDetailViewTest(APITestCase):
         self.gebruiker = GebruikerFactory.create()
         self.gebruiker.user.is_superuser = True
         self.gebruiker.user.save()
+        self.vak = VakFactory.create()
+        self.vak.studenten.add(self.gebruiker)
+        self.vak.save()
         self.client = APIClient()
         self.client.force_login(self.gebruiker.user)
         self.url = reverse("gebruiker_detail", kwargs={"id": self.gebruiker.user.id})
@@ -61,27 +67,17 @@ class GebruikerDetailViewTest(APITestCase):
     def test_gebruiker_detail_put(self):
         new_data = {
             "user": self.gebruiker.user.id,
-            "is_lesgever": not self.gebruiker.is_lesgever,
-            "gepinde_vakken": self.gebruiker.gepinde_vakken.all(),
+            "is_lesgever": self.gebruiker.is_lesgever,
+            "gepinde_vakken": [self.vak.vak_id],
         }
         response = self.client.put(self.url, new_data, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.gebruiker.refresh_from_db()
-        self.assertEqual(self.gebruiker.is_lesgever, new_data["is_lesgever"])
+        self.assertEqual(self.gebruiker.gepinde_vakken.first(), self.vak)
 
     def test_gebruiker_detail_get_non_existing_user(self):
         response = self.client.get(reverse("gebruiker_detail", kwargs={"id": 69}))
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-
-    def test_gebruiker_detail_get_unauthorized(self):
-        student = GebruikerFactory.create(is_lesgever=False)
-        self.gebruiker.user.is_superuser = False
-        self.gebruiker.user.save()
-        self.gebruiker.is_lesgever = False
-        self.gebruiker.save()
-        self.client.force_login(student.user)
-        response = self.client.get(self.url)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_gebruiker_detail_put_unauthorized(self):
         student = GebruikerFactory.create(is_lesgever=False)
@@ -103,12 +99,12 @@ class GebruikerDetailViewTest(APITestCase):
 
     def test_gebruiker_detail_patch(self):
         new_data = {
-            "is_lesgever": not self.gebruiker.is_lesgever,
+            "gepinde_vakken": [],
         }
         response = self.client.patch(self.url, new_data, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.gebruiker.refresh_from_db()
-        self.assertEqual(self.gebruiker.is_lesgever, new_data["is_lesgever"])
+        self.assertEqual(self.gebruiker.gepinde_vakken.all().count(), 0)
 
 
 class GebruikerDetailMeViewTest(APITestCase):
